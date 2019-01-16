7bb85e8339365f2df1160781d0844f925f119412
Francis King <francis.kingjr@gmail.com>
2019-01-16 17:52:04 -0500

    syntax fix

--- echoserver.py
+++ echoserver.py
@@ -20,7 +20,7 @@ def handle_verification():
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-  print "Handling Messages"
+  print("Handling Messages")
   payload = request.get_data()
   print payload
   for sender, message in messaging_events(payload):
